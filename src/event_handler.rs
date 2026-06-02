cad4a0530da62ff6a2b2319071d5b47e0826e3c9
Juan Ignacio Dominguez <ji.dominguez@globant.com>
2026-06-02 20:13:09 -0300

    feat(continous-deployment): add policy creation

--- src/event_handler.rs
+++ src/event_handler.rs
@@ -3,11 +3,16 @@ use aws_lambda_events::event::eventbridge::EventBridgeEvent;
 use std::env;
 use reqwest::header::{HeaderMap, HeaderValue, AUTHORIZATION, USER_AGENT, ACCEPT};
 use serde::{Serialize, Deserialize};
+use aws_sdk_cloudfront::types::{
+    ContinuousDeploymentPolicyConfig, ContinuousDeploymentPolicyType, ContinuousDeploymentSingleHeaderConfig, StagingDistributionDnsNames, TrafficConfig
+};
+use aws_sdk_cloudfront::Client as CfClient;
 
 // El payload que espera recibir tu Lambda
 #[derive(Serialize, Deserialize, Debug)]
 pub(crate) struct CustomEvent {
     blue_distribution_id: String,
+    green_distribution_dns_name: String,
     green_distribution_id: String,
     orchestrator_distribution_id: String,
     oac_id: String
@@ -64,6 +69,7 @@ pub(crate)async fn function_handler(event: LambdaEvent<EventBridgeEvent<CustomEv
             &event.payload.detail.green_distribution_id,
             &event.payload.detail.blue_distribution_id,
             &event.payload.detail.orchestrator_distribution_id,
+            &event.payload.detail.green_distribution_dns_name,
             &event.payload.detail.oac_id
         ).await
     });
@@ -95,8 +101,67 @@ async fn process_repo(
     green_distribution_id: &str,
     blue_distribution_id: &str,
     orchestrator_distribution_id: &str,
+    green_distribution_dns_name: &str,
     oac_id: &str
 ) -> Result<(), Error> {
+    // 1. Inicializar cliente de AWS CloudFront
+    let config = aws_config::load_from_env().await;
+    let cf_client = CfClient::new(&config);
+
+    let traffic_config = TrafficConfig::builder()
+        .r#type(ContinuousDeploymentPolicyType::SingleHeader)
+        .single_header_config(
+            ContinuousDeploymentSingleHeaderConfig::builder()
+                .header("aws-cf-cd-staging")
+                .value("true")
+                .build()
+                .unwrap()
+        ).build().unwrap();
+
+    // 2. Crear la Continuous Deployment Policy (vinculando la Green/Staging)
+    let policy_config = ContinuousDeploymentPolicyConfig::builder()
+        .enabled(true)
+        .traffic_config(traffic_config)
+        .staging_distribution_dns_names(StagingDistributionDnsNames::builder().quantity(1)
+                .items(green_distribution_dns_name.to_string())
+                .build().unwrap())
+        .build()?;
+
+    let create_policy_res = cf_client
+        .create_continuous_deployment_policy()
+        .continuous_deployment_policy_config(policy_config)
+        .send()
+        .await?;
+
+    let policy_id = create_policy_res
+        .continuous_deployment_policy()
+        .and_then(|p| Option::from(p.id.clone()))
+        .ok_or("No se pudo obtener el ID de la política creada")?;
+
+    // 3. Obtener la distribución de Producción (Blue) para poder actualizarla
+    let get_dist_res = cf_client
+        .get_distribution()
+        .id(blue_distribution_id)
+        .send()
+        .await?;
+
+    let etag = get_dist_res.e_tag().ok_or("No ETag found")?.to_string();
+    let distribution = get_dist_res.distribution().ok_or("No distribution found")?;
+    let dist_config = distribution.distribution_config().ok_or("No config found")?;
+
+    // 4. Clonar y mutar la configuración para inyectar la política
+    let mut updated_config = dist_config.clone();
+    updated_config.continuous_deployment_policy_id = Some(policy_id.to_string());
+
+    // 5. Actualizar la distribución de producción en AWS
+    cf_client
+        .update_distribution()
+        .id(blue_distribution_id)
+        .distribution_config(updated_config)
+        .if_match(etag) // Requerido por CloudFront
+        .send()
+        .await?;
+
     // Paso B: Disparar Workflow Dispatch
     let dispatch_url = format!(
         "https://api.github.com/repos/{}/{}/actions/workflows/deploy.yml/dispatches", 
